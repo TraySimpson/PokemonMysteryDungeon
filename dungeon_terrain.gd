0888f573c344efad645c191629303fc53263ac26
Tray Simpson <tdsimpson123@gmail.com>
2024-09-20 17:43:37 -0600

    Fix: 400 errors fixed, adds water, wall and ground tile coords

--- dungeon_terrain.gd
+++ dungeon_terrain.gd
@@ -5,13 +5,16 @@ extends Node2D
 @export var map_height: int = 10
 
 @onready var ground_layer: TileMapLayer = $GroundLayer
-
+const WALL_TILE: Vector2 = Vector2(1 + (0 * 6), 1)
+const WATER_TILE: Vector2 = Vector2(1 + (1 * 6), 1)
+const FLOOR_TILE: Vector2 = Vector2(1 + (2 * 6), 1)
 
 func _ready() -> void:
 	ground_layer.tile_set = tileset
 	create_dungeon()
 
 func create_dungeon() -> void:
+	print("Drawing dungeon")
 	for x in range(map_width):
 		for y in range(map_height):
-			ground_layer.set_cell(Vector2(x, y), 1, Vector2(1 + (2 * 6), 1))
+			ground_layer.set_cell(Vector2(x, y), tileset.get_source_id(0), WALL_TILE)
